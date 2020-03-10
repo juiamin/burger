37124ef5455b31de1516049d372281a274aba8f0
jui patel <amin.jui2712@gmail.com>
2020-03-10 12:47:01 -0400

    created schema

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN DEFAULT false NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO burgers
+  (burger_name)
+VALUES
+  ('veggi burger'),
+  ('paneer burger'),
+  ('Aloo burger');
+
+SELECT * FROM burgers;
